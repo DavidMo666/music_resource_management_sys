c57ce2127de7ebbfdcd5c28cf479c412d8b365f8
DavidMo666 <151990218+DavidMo666@users.noreply.github.com>
2025-03-22 18:33:18 +0800

    初始

---
+++ src/main/java/com/g12/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.g12.service.impl;
+
+import com.g12.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
