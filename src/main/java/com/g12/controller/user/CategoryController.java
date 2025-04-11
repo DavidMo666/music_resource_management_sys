a401018278d568f76c48c1fffcf8c9938d68707a
Siwei <1525489345@qq.com>
2025-04-11 15:45:31 +0800

    イフ

--- src/main/java/com/g12/controller/user/CategoryController.java
+++ src/main/java/com/g12/controller/user/CategoryController.java
@@ -1,9 +1,35 @@
 package com.g12.controller.user;
 
+import com.g12.entity.MusicCategory;
+import com.g12.result.Result;
+import com.g12.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user/category")
 public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category 分类信息
+     * @return 操作结果
+     */
+    @PostMapping
+    public Result<String> add(@RequestBody MusicCategory category) {
+        // 验证分类名称不能为空
+        if (category.getName() == null || category.getName().trim().isEmpty()) {
+            return Result.error("分类名称不能为空");
+        }
+        
+        // 调用服务层保存分类
+        categoryService.save(category);
+        return Result.success("分类添加成功");
+    }
 }
--- src/main/java/com/g12/service/impl/CategoryServiceImpl.java
+++ src/main/java/com/g12/service/impl/CategoryServiceImpl.java
@@ -1,8 +1,19 @@
 package com.g12.service.impl;
 
+import com.g12.entity.MusicCategory;
+import com.g12.mapper.CategoryMapper;
 import com.g12.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
+
+    @Autowired
+    private CategoryMapper categoryMapper;
+
+    @Override
+    public void save(MusicCategory category) {
+        categoryMapper.insert(category);
+    }
 }
